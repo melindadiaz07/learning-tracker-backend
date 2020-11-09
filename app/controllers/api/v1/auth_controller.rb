8792fa8fc1a5ede354fc1dc3598f896bf70ffc5c
alexstephane <alexngundji@gmail.com>
2020-11-09 17:42:03 -0500

    wip

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::AuthController < ApplicationController
+
+    def create
+      user = User.find_by(name: params[:username])
+  
+      if user && user.authenticate(params[:password])
+       
+        token = issue_token(user)
+        render json: {id: user.id, username: user.name, jwt: token}
+      else
+        render json: {error: 'That user could not be found'}, status: 401
+      end
+    end
+
+
+    # def create # POST /api/v1/login
+
+    #     @user = User.find_by(username: params[:username])
+    #     if @user && @user.authenticate(params[:password])
+    #       token = encode("user_id": @user.id)
+    #       render json: {
+    #         authenticated: true,
+    #         message: "login success",
+    #         token: token,
+    #         user: @user,
+    #       }, status: :accepted
+    #     else
+    #         render json: {
+    #         authenticated: false,
+    #         message: "login failed"
+    #       }, status: :unauthorized 
+    #     end
+    #   end
+
+    
+  
+    def show
+      token = request.headers['Authorization']
+      user = User.find_by(id: token)
+      if logged_in?
+        render json: { id: current_user.id, username: current_user.username }
+      else
+        render json: {error: 'No user could be found'}, status: 401
+      end
+    end
+  
+  
+  
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :courses
   resources :task_lists
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      post '/auth', to: 'auth#create'
+     get '/current_user', to: 'auth#show'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
