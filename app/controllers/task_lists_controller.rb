a447c6131e177a2c26f55387f3d64fe50e09af71
Melindadiaz07 <melindadiaz@me.com>
2020-11-09 11:26:39 -0500

    Updated controllers to handle fetch patch call

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,4 +1,16 @@
 class TaskListsController < ApplicationController
  
+  def index
+    render json: TaskList.all.to_json(:only => [:id, :tasks])
+  end
+  
+  def show
+    render json: TaskList.find(params[:id]).to_json(:only => [:id, :tasks])
+  end
+
+  def update
+    taskList = TaskList.find(params[:id])
+    taskList.update(tasks: params[:tasks])
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
     def index 
       render json: User.all.to_json(:include => { :courses => {
         :include => { :task_list => {
-                        :only => :tasks } },
+                        :only => [:id, :tasks]} },
         :only => [:id, :subject]} })
     end
 
     def show
       render json: User.find(params[:id]).to_json(:include => { :courses => {
         :include => { :task_list => {
-                        :only => :tasks } },
+                        :only => [:id, :tasks] } },
         :only => [:id, :subject]} })
     end
 
