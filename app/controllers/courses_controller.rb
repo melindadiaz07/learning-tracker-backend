663b72884077bff1b461208ea14de40de7143d15
Melindadiaz07 <melindadiaz@me.com>
2020-11-10 18:59:44 -0500

    Fixed courses controller to render correct json

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,10 +11,9 @@ class CoursesController < ApplicationController
 
   def create
     @course = Course.create(subject: params[:subject], user_id: params[:user_id])
-    TaskList.create(tasks: [params[:tasks]], course_id: @course.id)
+    TaskList.create(tasks: params[:tasks], course_id: @course.id)  
+    render json: @course.to_json(:include => {:task_list => {:only => [:id, :tasks]}}) 
   end
-
-  
 
 
 end
